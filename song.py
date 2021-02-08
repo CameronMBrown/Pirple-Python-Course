bcad4a809b3326172bcc1e8cf982672d9ad2fe1e
CameronMBrown <53124876+CameronMBrown@users.noreply.github.com>
2021-02-08 12:53:26 -0500

    Add files via upload

---
+++ song.py
@@ -0,0 +1,24 @@
+#Pirple Assignment 1 - variables
+
+#favourite song, attributes as unique varables
+name = "Dear Maria, Count Me In" #String - song name
+artist = "All Time Low" #String - band name
+album = "So Wrong, It's Right" #String - album name
+genre = "Pop Punk" #String - song genre
+label = "Hopeless Records" #String - record label
+year = 2008 #Integer - release year
+playtime = "3:02" #String - track length 
+seconds = 182 #Integer - playtime in seconds
+hours = 0.051 #Integer - playtime in hours
+
+
+print(name)
+print(artist)
+print(album)
+print(genre)
+print(label)
+print(year)
+print(playtime)
+print(seconds)
+print(hours)
+
