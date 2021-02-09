60569afcee77796fb4b938038511f056d71b4773
CameronMBrown <53124876+CameronMBrown@users.noreply.github.com>
2021-02-09 11:53:06 -0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,19 @@
+#Pirple Python Assignment 2 
+#funtions and returns
+
+def name():
+  return "Dear Maria, Count Me In"
+
+def artist():
+  return "All Time Low"
+
+def year():
+  return 2008
+
+def isAwesome():
+  return True
+
+print(name())
+print(artist())
+print(year())
+print("Is this song Awesome?", isAwesome())
